d718ea40cbbf583984b74a20854fb75cdcc24051
Olexander <73075612+alexkrnnk@users.noreply.github.com>
2023-02-08 16:05:18 +0200

    Create the Model for coin dates

---
+++ MVVM/Model/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCryptApp.MVVM.Model
+{
+    sealed class Coin
+    {
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Rank { get; set; }
+
+        public double VolumeUsd { get; set; }
+
+        public double PriceUsd { get; set; }
+
+        public double PriceChange { get; set; }
+
+        public string? WebSite { get; set; }
+    }
+}
